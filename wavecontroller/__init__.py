a1a3d205778328df882676efc456162ad45cc1fa
oparada1988 <oparada1988@gmail.com>
2026-08-24 12:17:45 -0400

    chore(release): bump WaveController to pre-alpha 6 (0.0.0.6)

--- wavecontroller/__init__.py
+++ wavecontroller/__init__.py
@@ -1,5 +1,5 @@
 # WaveController Version Specification: public.beta.alpha.pre-alpha
-__version__ = "0.0.0.5"
-__build__ = "0.0.0.5"
+__version__ = "0.0.0.6"
+__build__ = "0.0.0.6"
 __github__ = "https://github.com/oparada1988/WaveController"
 __issues__ = "https://github.com/oparada1988/WaveController/issues"
